6088e769d5d75d0983f09a7528c5dcbbd54fa03e
Haley Warson <haleywarson@gmail.com>
2021-05-10 15:14:00 -0600

    Added color palatte to css.

--- pack-planit/src/Components/Plan.js
+++ pack-planit/src/Components/Plan.js
@@ -12,28 +12,12 @@ export default function Plan() {
     const [submitted, setSubmitted] = useState(false);
     const [valid, setValid] = useState(false);
 
-    const handleTripNameChange = (event) => {
+    const handleInputChange = (event) => {
         event.persist();
-        setValues((values) => ({
-            ...values,
-            tripName: event.target.value,
-        }));
-    };
-
-    const handleTripLocationChange = (event) => {
-        event.persist();
-        setValues((values) => ({
-            ...values,
-            tripLocation: event.target.value,
-        }));
-    };
-
-    const handleTripTypeChange = (event) => {
-        event.persist();
-        setValues((values) => ({
-            ...values,
-            tripType: event.target.value,
-        }));
+        // setValues({
+        //     // use spread operator here
+        //     event.target.name: event.target.value
+        // })
     };
     
     const handleSubmit = (event) => {
@@ -42,61 +26,70 @@ export default function Plan() {
             setValid(true);
         }
         setSubmitted(true);
+        showSuccess();
+        console.log("submitted")
         };
+
+    const showSuccess = () => {
+        return (
+            <div className="success-message">
+                <p>You are on your way to planning your trip!</p>
+            </div>
+        )
+    }
 
     return (
         <>
             <div className="plan-container">
-            <form className="plan-form" onSubmit={handleSubmit}>
-                <h1>Plan a trip</h1>
+                <form className="plan-form" onSubmit={handleSubmit}>
+                    <h1>Plan a trip</h1>
 
-                <input
-                id="trip-name"
-                className="form-field"
-                type="text"
-                // disabled={showSuccess}
-                placeholder="Trip name"
-                name="tripName"
-                value={values.tripName}
-                onChange={handleTripNameChange}
-                />
-                {submitted && !values.tripName && (
-                <span id="trip-name-error">Please enter a trip name.</span>
-                )}
+                    <input
+                    id="trip-name"
+                    className="form-field"
+                    type="text"
+                    // disabled={showSuccess}
+                    placeholder="Trip name"
+                    name="tripName"
+                    value={values.tripName}
+                    onChange={handleInputChange}
+                    />
+                    {submitted && !values.tripName && (
+                    <span id="trip-name-error">Please enter a trip name.</span>
+                    )}
 
-                <input
-                id="trip-location"
-                className="form-field"
-                type="text"
-                placeholder="Trip location"
-                name="tripLocation"
-                value={values.tripLocation}
-                onChange={handleTripLocationChange}
-                />
-                {submitted && !values.tripLocation && (
-                <span id="trip-location-error">
-                    Please enter a trip location.
-                </span>
-                )}
+                    <input
+                    id="trip-location"
+                    className="form-field"
+                    type="text"
+                    placeholder="Trip location"
+                    name="tripLocation"
+                    value={values.tripLocation}
+                    onChange={handleInputChange}
+                    />
+                    {submitted && !values.tripLocation && (
+                    <span id="trip-location-error">
+                        Please enter a trip location.
+                    </span>
+                    )}
 
-                <input
-                id="trip-type"
-                className="form-field"
-                type="text"
-                placeholder="Trip type"
-                name="tripType"
-                value={values.tripType}
-                onChange={handleTripTypeChange}
-                />
+                    <input
+                    id="trip-type"
+                    className="form-field"
+                    type="text"
+                    placeholder="Trip type"
+                    name="tripType"
+                    value={values.tripType}
+                    onChange={handleInputChange}
+                    />
 
-                <input type="submit" value="Submit"></input>
-                
-                {submitted && !values.tripType && (
-                <span id="trip-type-error">Please enter a trip type.</span>
-                )}
-            </form>
+                    <input type="submit" value="Submit"></input>
+                    
+                    {submitted && !values.tripType && (
+                    <span id="trip-type-error">Please enter a trip type.</span>
+                    )}
+                </form>
             </div>
-            {/* {showSuccess && <div className='success-message'>Success! Thank you for registering</div>} */}
         </>
     );
 }
