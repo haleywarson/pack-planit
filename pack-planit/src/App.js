4feccac3b5c03b358c7bb839327a840ebba46781
Haley Warson <haleywarson@gmail.com>
2021-05-10 10:42:32 -0600

    Routes working

--- pack-planit/src/App.js
+++ pack-planit/src/App.js
@@ -1,10 +1,36 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import "./App.css";
+
+import Plan from "./Plan"
+import Home from "./Home"
 
 function App() {
   return (
-    <div className="App">
-      Hello World.
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/plan">Plan your trip</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/plan">
+            <Plan />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ pack-planit/src/Plan.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Plan() {
+    return (
+        <div>
+            <h2>Plan your trip</h2>
+        </div>
+    )
+}
