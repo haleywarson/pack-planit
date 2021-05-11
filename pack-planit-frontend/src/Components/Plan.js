bd0f17c97916fc3c0303a6b5a4f4138777a38bc9
Haley Warson <haleywarson@gmail.com>
2021-05-11 14:59:05 -0600

    Form inputs working with state. Form fields updated.

--- pack-planit-frontend/src/Components/Plan.js
+++ pack-planit-frontend/src/Components/Plan.js
@@ -4,103 +4,159 @@ import "../App.css";
 
 export default function Plan() {
     const [values, setValues] = useState({
-        tripName: "",
-        tripLocation: "",
-        tripType: "",
+        name: "",
+        location: "",
+        image: "",
+        startDate: "",
+        endDate: "",
+        category: "",
+        miles: 0,
+        difficulty: "",
+        rating: 0,
+        notes: ""
     });
-    // const [submitted, setSubmitted] = useState(false);
-    // const [valid, setValid] = useState(false);
 
     const handleInputChange = (event) => {
         event.persist();
-        let newValue = event.target.value
+        const name = event.target.name;
+        const value = event.target.value;
         setValues({
-            [event.target.name]: [ ...values, newValue ]
+            ...values,
+            [name]: value
         })
     }
     
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log("submitted")
-        // if (values.tripName && values.tripLocation && values.tripType) {
-        //     setValid(true);
-        // }
-        // setSubmitted(true);
-        // showSuccess();
-        };
-
-    // const showSuccess = () => {
-    //     return (
-    //         <div className="success-message">
-    //             <p>You are on your way to planning your trip!</p>
-    //         </div>
-    //     )
-    // }
+        setValues({
+            [event.target.name]: event.target.value,
+        });
+    };
 
     return (
         <>
             <div className="plan-container">
-                <form className="plan-form" onSubmit={handleSubmit}>
-                    <h1>Plan a trip</h1>
+            <form className="plan-form" onSubmit={handleSubmit}>
+                <h1>Log a trip</h1>
 
-                    <input
-                    id="trip-name"
-                    className="form-field"
-                    type="text"
-                    // disabled={showSuccess}
-                    placeholder="Trip name"
-                    name="tripName"
-                    value={values.tripName}
-                    onChange={handleInputChange}
-                    />
-                    {/* {submitted && !values.tripName && (
-                    <span id="trip-name-error">Please enter a trip name.</span>
-                    )} */}
+                <label for="name">Trip name</label>
+                <input
+                id="name"
+                className="form-field"
+                type="text"
+                placeholder=""
+                name="name"
+                value={values.name}
+                onChange={handleInputChange}
+                />
 
-                    <input
-                    id="trip-location"
-                    className="form-field"
-                    type="text"
-                    placeholder="Trip location"
-                    name="tripLocation"
-                    value={values.tripLocation}
-                    onChange={handleInputChange}
-                    />
-                    {/* {submitted && !values.tripLocation && (
-                    <span id="trip-location-error">
-                        Please enter a trip location.
-                    </span>
-                    )} */}
+                <label for="location">Trip location</label>
+                <input
+                id="location"
+                className="form-field"
+                type="text"
+                placeholder=""
+                name="location"
+                value={values.location}
+                onChange={handleInputChange}
+                />
 
-                    <input
-                    id="trip-type"
-                    className="form-field"
-                    type="text"
-                    placeholder="Trip type"
-                    name="tripType"
-                    value={values.tripType}
-                    onChange={handleInputChange}
-                    />
+                <label for="image">Trip image</label>
+                <input
+                id="image"
+                className="form-field"
+                type="text"
+                placeholder="image url"
+                name="image"
+                value={values.image}
+                onChange={handleInputChange}
+                />
 
-                    <input type="submit" value="Submit"></input>
-                    
-                    {/* {submitted && !values.tripType && (
-                    <span id="trip-type-error">Please enter a trip type.</span>
-                    )} */}
-                </form>
+                <label for="startDate">Start date</label>
+                <input
+                id="startDate"
+                className="form-field"
+                type="date"
+                placeholder="Start date"
+                name="startDate"
+                value={values.startDate}
+                onChange={handleInputChange}
+                />
+
+                <label for="endDate">End date</label>
+                <input
+                id="endDate"
+                className="form-field"
+                type="date"
+                placeholder="End date"
+                name="endDate"
+                value={values.endDate}
+                onChange={handleInputChange}
+                />
+
+                <label for="category">Category (i.e. backpacking/camping)</label>
+                <input
+                id="category"
+                className="form-field"
+                type="text"
+                placeholder=""
+                name="category"
+                value={values.category}
+                onChange={handleInputChange}
+                />
+
+                <label for="miles">Number of miles</label>
+                <input
+                id="miles"
+                className="form-field"
+                type="number"
+                placeholder=""
+                name="miles"
+                value={values.miles}
+                onChange={handleInputChange}
+                />
+
+                <label for="difficulty">
+                Difficulty (i.e. easy/moderate/difficult)
+                </label>
+                <input
+                id="difficulty"
+                className="form-field"
+                type="text"
+                placeholder=""
+                name="difficulty"
+                value={values.difficulty}
+                onChange={handleInputChange}
+                />
+
+                <label for="rating">
+                Rating (1-5 stars, with 5 being the best)
+                </label>
+                <input
+                id="rating"
+                className="form-field"
+                type="range"
+                placeholder="Rating"
+                name="rating"
+                value={values.rating}
+                onChange={handleInputChange}
+                />
+
+                <label for="notes">Trip notes</label>
+                <input
+                id="notes"
+                className="form-field"
+                type="text"
+                placeholder=""
+                name="notes"
+                value={values.notes}
+                onChange={handleInputChange}
+                />
+
+                <input type="submit" value="Submit"></input>
+            </form>
             </div>
         </>
     );
 }
-
-// t.string :name
-//       t.string :location
-//       t.date :start_date
-//       t.date :end_date
-//       t.string :category
-//       t.integer :length
-//       t.string :difficulty
-//       t.integer :rating
-//       # add image url
-//       # add notes section
-//       # add foreign key?
