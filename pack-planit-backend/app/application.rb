8b455844c293b3f955a9fd21f1c171a1b79d1489
Haley Warson <haleywarson@gmail.com>
2021-05-11 13:45:29 -0600

    All routes CRUD functioning properly.

--- pack-planit-backend/app/application.rb
+++ pack-planit-backend/app/application.rb
@@ -7,26 +7,30 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/trips/) && req.get?
-      return index 
-    elsif req.path.match(/trips/) && req.post?
-      trip = JSON.parse(req.body.read)
-      new_trip = Trip.create(trip)
-      return create(new_trip)
+    if req.path == '/trips' && req.get?
+        return [200, {'Content-Type' => 'application/json'}, [{trips: Trip.all}.to_json]]
+    elsif req.path.match(/trips/) && req.get?
+        id = req.path.split('/')[2]
+        trip = Trip.find(id)
+        return [200, {'Content-Type' => 'application/json'}, [trip.to_json]]
+    elsif req.path == '/trips' && req.post?
+      data = JSON.parse(req.body.read)
+      new_trip = Trip.create(data)
+      return [201, {'Content-Type' => 'application/json'}, [trip.to_json]]
+    elsif req.path.match(/trips/) && req.patch?
+        id = req.path.split('/')[2]
+        trip = Trip.find(id)
+        data = JSON.parse(req.body.read)
+        trip.update(data)
+        return [200, {'Content-Type' => 'application/json'}, [trip.to_json]]
+    elsif req.path.match(/trips/) && req.delete?
+        id = req.path.split('/')[2]
+        trip = Trip.find(id)
+        trip.destroy 
+        return [204, {}, ['']]
+    else
+      return [404, {}, ['']]
     end 
+
   end
-
-  def index
-    [200, {'Content-Type' => 'application/json'}, [{trips: Trip.all}.to_json]]
-  end 
-
-  def show trip 
-    [200, {'Content-Type' => 'application/json'}, [trip]]
-  end 
-
-  def create trip 
-    [200, {'Content-Type' => 'application/json'}, [trip.to_json]]
-  end 
-
-
 end
