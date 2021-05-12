2546cecbf0199f6180529f6328f6dd0b386b118a
Haley Warson <haleywarson@gmail.com>
2021-05-12 14:16:55 -0600

    Logo linking to home page.

--- pack-planit-frontend/src/App.js
+++ pack-planit-frontend/src/App.js
@@ -9,35 +9,15 @@ import PackItForm from "./Components/PackItForm"
 
 function App() {
 
-const toggleActive = (e) => {
-  e.preventDefault();
-  // menu-btn-active.classList.toggle("active"); 
-  // nav-menu-active.classList.toggle("active"); 
-}
-
   return (
     <Router>
       <div>
         <header>
-          <h1 className="title">Pack Planit</h1>
-          <button
-            className="menu-btn-active"
-            // onClick={toggleActive}
-          >
-            <span className="menu-line"></span>
-            <span className="menu-line"></span>
-            <span className="menu-line"></span>
-          </button>
-          <nav
-            className="nav-menu-active"
-            // onClick={toggleActive}
-          >
+          <Link to="/" >
+            <h1 id="logo">Pack Planit</h1>
+          </Link>
+          <nav className="nav-menu-active">
             <ul className="nav-list">
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  Home
-                </Link>
-              </li>
               <li className="nav-item">
                 <Link to="/packit" className="nav-link">
                   Plan your trip
