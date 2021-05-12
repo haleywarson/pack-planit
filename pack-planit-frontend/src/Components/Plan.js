591371f2cec18a5fb156b95fa3071626e381c56d
Haley Warson <haleywarson@gmail.com>
2021-05-12 10:50:41 -0600

    Updated labels in form.

--- pack-planit-frontend/src/Components/Plan.js
+++ pack-planit-frontend/src/Components/Plan.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 import "../App.css";
 
+const tripsUrl = "http://localhost:9393/trips"
+
 export default function Plan() {
     const [values, setValues] = useState({
         name: "",
@@ -28,135 +30,141 @@ export default function Plan() {
     
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log("submitted")
-        setValues({
-            [event.target.name]: event.target.value,
-        });
+        console.log("submitting...")
+        const name = event.target.name
+        const value = event.target.value
+        const newValues = setValues({ [name]: value });
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newValues)
+        }
+        fetch(tripsUrl, options)
     };
 
     return (
-        <>
-            <div className="plan-container">
+        <div className="plan-container">
             <form className="plan-form" onSubmit={handleSubmit}>
-                <h1>Log a trip</h1>
+            <h1>Log a trip</h1>
 
-                <label for="name">Trip name</label>
-                <input
+            <label htmlFor="name">Trip name</label>
+            <input
                 id="name"
                 className="form-field"
                 type="text"
                 placeholder=""
                 name="name"
-                value={values.name}
+                value={values.name || ""}
                 onChange={handleInputChange}
-                />
+            />
 
-                <label for="location">Trip location</label>
-                <input
+            <label htmlFor="location">Trip location</label>
+            <input
                 id="location"
                 className="form-field"
                 type="text"
                 placeholder=""
                 name="location"
-                value={values.location}
+                value={values.location || ""}
                 onChange={handleInputChange}
-                />
+            />
 
-                <label for="image">Trip image</label>
-                <input
+            <label htmlFor="image">Trip image</label>
+            <input
                 id="image"
                 className="form-field"
                 type="text"
                 placeholder="image url"
                 name="image"
-                value={values.image}
+                value={values.image || ""}
                 onChange={handleInputChange}
-                />
+            />
 
-                <label for="startDate">Start date</label>
-                <input
+            <label htmlFor="startDate">Start date</label>
+            <input
                 id="startDate"
                 className="form-field"
                 type="date"
                 placeholder="Start date"
                 name="startDate"
-                value={values.startDate}
+                value={values.startDate || ""}
                 onChange={handleInputChange}
-                />
+            />
 
-                <label for="endDate">End date</label>
-                <input
+            <label htmlFor="endDate">End date</label>
+            <input
                 id="endDate"
                 className="form-field"
                 type="date"
                 placeholder="End date"
                 name="endDate"
-                value={values.endDate}
+                value={values.endDate || ""}
                 onChange={handleInputChange}
-                />
+            />
 
-                <label for="category">Category (i.e. backpacking/camping)</label>
-                <input
+            <label htmlFor="category">Category (i.e. backpacking/camping)</label>
+            <input
                 id="category"
                 className="form-field"
                 type="text"
                 placeholder=""
                 name="category"
-                value={values.category}
+                value={values.category || ""}
                 onChange={handleInputChange}
-                />
+            />
 
-                <label for="miles">Number of miles</label>
-                <input
+            <label htmlFor="miles">Number of miles</label>
+            <input
                 id="miles"
                 className="form-field"
                 type="number"
                 placeholder=""
                 name="miles"
-                value={values.miles}
+                value={values.miles || 0}
                 onChange={handleInputChange}
-                />
+            />
 
-                <label for="difficulty">
+            <label htmlFor="difficulty">
                 Difficulty (i.e. easy/moderate/difficult)
-                </label>
-                <input
+            </label>
+            <input
                 id="difficulty"
                 className="form-field"
                 type="text"
                 placeholder=""
                 name="difficulty"
-                value={values.difficulty}
+                value={values.difficulty || ""}
                 onChange={handleInputChange}
-                />
+            />
 
-                <label for="rating">
+            <label htmlFor="rating">
                 Rating (1-5 stars, with 5 being the best)
-                </label>
-                <input
+            </label>
+            <input
                 id="rating"
                 className="form-field"
                 type="range"
                 placeholder="Rating"
                 name="rating"
-                value={values.rating}
+                value={values.rating || 0}
                 onChange={handleInputChange}
-                />
+            />
 
-                <label for="notes">Trip notes</label>
-                <input
+            <label htmlFor="notes">Trip notes</label>
+            <input
                 id="notes"
                 className="form-field"
                 type="text"
                 placeholder=""
                 name="notes"
-                value={values.notes}
+                value={values.notes || ""}
                 onChange={handleInputChange}
-                />
+            />
 
-                <input type="submit" value="Submit"></input>
+            <input type="submit" value="Submit"></input>
             </form>
-            </div>
-        </>
+        </div>
     );
 }
