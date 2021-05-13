0c8ff3e2de0cd68de0bb7718d8b9f5142b7386eb
Haley Warson <haleywarson@gmail.com>
2021-05-13 17:58:09 -0600

    Packing list fields styled.

--- pack-planit-frontend/src/Components/Main.js
+++ pack-planit-frontend/src/Components/Main.js
@@ -11,7 +11,7 @@ export default function Main() {
                 <div className="main-content">
                     <h2>YOUR NEXT TRIP AWAITS</h2>
                     <h3>Let's help you plan it.</h3>
-                    <button><a href="http://localhost:3000/packit" target="_blank">plan your Trip</a></button>
+                    <button><a href="http://localhost:3000/packit">plan your Trip</a></button>
                 </div>
             </div>
             <div className="trips-container">
